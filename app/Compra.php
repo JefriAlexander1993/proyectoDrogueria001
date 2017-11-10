c917d38650331e85bd27505c485663336b82532d
Diego Nuñez <diego.nm97@hotmail.com>
2017-11-09 23:24:31 -0500

    'CompraSinBusqueda'

--- app/Compra.php
+++ app/Compra.php
@@ -9,10 +9,7 @@ class Compra extends Model
 
 
     protected $table = 'compras';
-    protected $fillable=['totalCompra'
-
-
-    ];
+    protected $fillable=['totalCompra'];
       
     public function articulo()
     {
@@ -23,4 +20,11 @@ class Compra extends Model
     {
         return $this->belongsTo('App\Proveedor');
     }
+
+public function scopeSearch($query, $codigo){
+
+return $query->where('codigo','LIKE',"%$codigo%");
+// return $query->where('type', $type);
+    }
+    
 }
